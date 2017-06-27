a634b4b1e97c2f55bbb1afb78cc41f41fd3bf020
Sapphire <saphmb@gmail.com>
2017-06-27 14:36:35 +0100

    Alternative rspec method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,37 +1,37 @@
-require_relative "../lib/fizzbuzz.rb"
+require "fizzbuzz.rb"
 
 describe 'fizzbuzz' do
     it "returns 'fizz' when passing 3" do
-      expect(fizzbuzz(3)).to eq 'fizz'
+      expect(3.fizzbuzz).to eq 'fizz'
     end
 end
 
 describe 'fizzbuzz' do
     it "returns 'fizz' when passing 9" do
-      expect(fizzbuzz(9)).to eq 'fizz'
+      expect(9.fizzbuzz).to eq 'fizz'
     end
 end
 
 describe 'fizzbuzz' do
     it "returns 'buzz' when passing 5" do
-      expect(fizzbuzz(5)).to eq 'buzz'
+      expect(5.fizzbuzz).to eq 'buzz'
     end
 end
 
 describe 'fizzbuzz' do
     it "returns 'buzz' when passing 10" do
-      expect(fizzbuzz(10)).to eq 'buzz'
+      expect(10.fizzbuzz).to eq 'buzz'
     end
 end
 
 describe 'fizzbuzz' do
     it "returns 'fizzbuzz' when passing 15" do
-      expect(fizzbuzz(15)).to eq 'fizzbuzz'
+      expect(15.fizzbuzz).to eq 'fizzbuzz'
     end
 end
 
 describe 'fizzbuzz' do
     it "returns 4 when passing 4" do
-      expect(fizzbuzz(4)).to eq 4
+      expect(4.fizzbuzz).to eq 4
     end
 end
